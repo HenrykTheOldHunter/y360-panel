c673c0bcc95f88b8def022dffc7b8c5071e40a41
HenrykTheOldHunter <71096407+HenrykTheOldHunter@users.noreply.github.com>
2024-05-16 15:37:36 +0300

    Add files via upload

---
+++ main_settings.py
@@ -0,0 +1,18 @@
+"""
+Production settings
+Should be kept in production secret
+"""
+
+SECRET_KEY = ''
+
+#Replace on organization token
+API_KEY = '' 
+
+#Replace on organization ID
+ORG_ID = ''
+
+#Switch on only while developing
+DEBUG = False
+
+#Hosts
+ALLOWED_HOSTS = []
